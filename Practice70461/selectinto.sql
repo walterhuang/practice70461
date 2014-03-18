866b2908a20850e3de7516b12bb2fd80ce74d959
walterhuang <isponder@gmail.com>
2014-03-19 01:26:14 +0800

    pilot

---
+++ Practice70461/selectinto.sql
@@ -0,0 +1,22 @@
+﻿--http://www.blackwasp.co.uk/SQLSelectInsert.aspx
+
+create table dbo.Person
+(
+Id int primary key,
+FirstName nvarchar(25) not null,
+LastName nvarchar(25)
+)
+
+insert dbo.Person values(1, 'walt', 'huang')
+insert into dbo.Person values(2, 'chia', 'li')
+
+select *
+into dbo.Employee
+from dbo.Person p
+where p.Id = 2
+
+select * from dbo.Person
+select * from dbo.Employee
+
+drop table dbo.Person
+drop table dbo.Employee
---
+++ Practice70461/xml.sql
@@ -0,0 +1,12 @@
+﻿--create table #t
+
+--insert into #t
+select * 
+into #t 
+from (values(1,'a'), (2,'b'), (3,'c')) t(c1,c2)
+
+select * from #t for xml raw
+select * from #t for xml raw, elements
+select * from #t for xml auto
+
+drop table #t
