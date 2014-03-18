25f1980cde8da92ee061f020ad14894c0a3cc130
walterhuang <isponder@gmail.com>
2014-03-19 01:43:17 +0800

    complete xml

--- Practice70461/xml.sql
+++ Practice70461/xml.sql
@@ -1,12 +1,18 @@
-﻿--create table #t
-
---insert into #t
+﻿
+with t as (
 select * 
-into #t 
-from (values(1,'a'), (2,'b'), (3,'c')) t(c1,c2)
+from (values(1,'a')/*, (2,'b'), (3,'c')*/) t(c1,c2)
+), x1(cx) as(
+select * from t for xml raw
+), x2(cx) as(
+select * from t for xml raw, elements
+), x3(cx) as(
+select * from t for xml auto
+), x4(cx) as(
+select * from t for xml auto, elements
+)
+select 'for xml raw', * from x1 union all
+select 'for xml raw, elements', * from x2 union all
+select 'for xml auto', * from x3 union all
+select 'for xml auto, elements', * from x4 
 
-select * from #t for xml raw
-select * from #t for xml raw, elements
-select * from #t for xml auto
-
-drop table #t
