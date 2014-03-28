36f370629c367b51738e5e931da4a2281be807e6
walterhuang <isponder@gmail.com>
2014-03-29 01:47:27 +0800

    lag

---
+++ Practice70461/lag.sql
@@ -0,0 +1,10 @@
+﻿
+-- http://msdn.microsoft.com/en-us/library/hh231256.aspx
+
+select *,
+lag(c2,1,'') over (order by c1) 
+from (values(1,'a'), (2,'b'), (3,'c')) t(c1,c2)
+
+select *,
+lag(c2) over (partition by c1 order by c2) 
+from (values(1,'a'),(1,'b'),(2,'c'),(2,'d'),(2,'e')) t(c1,c2)
